9a1660c8cccb60da83c9b8fc57497e9f3ad4f37b
Shanto93 <shantoislam7363@gmail.com>
2024-07-05 10:17:33 +0600

    menu item is added to homepage

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -2,33 +2,31 @@ import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../shared/MenuItem/MenuItem";
 
-
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
+  const [menu, setMenu] = useState([]);
 
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const dataItems = data.filter((item) => item.category === "popular");
+        setMenu(dataItems);
+      });
+  }, []);
+  return (
+    <section>
+      <SectionTitle
+        subheading={"---Check it out---"}
+        heading={"FROM OUR MENU"}
+      ></SectionTitle>
 
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res => res.json())
-        .then(data => {const dataItems = data.filter(item => item.category ==='popular');
-            setMenu(dataItems);
-
-        })
-    },[])
-    return (
-        <section>
-            <SectionTitle
-            subheading={"---Check it out---"}
-            heading={"FROM OUR MENU"}
-            >
-            </SectionTitle>
-
-            {
-                menu.map(item => <MenuItem key={item.key} item={item}></MenuItem>)
-            }
-           
-        </section>
-    );
+      <div className="mb-10 grid md:grid-cols-2 gap-10">
+        {menu.map((item) => (
+          <MenuItem key={item.key} item={item}></MenuItem>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
--- src/pages/shared/MenuItem/MenuItem.jsx
+++ src/pages/shared/MenuItem/MenuItem.jsx
@@ -1,13 +1,14 @@
 const MenuItem = ({ item }) => {
   const { image, price, name, recipe } = item;
   return (
-    <div>
-      <img src={image} alt="Popular Menu Image" />
+    <div className="flex space-x-5">
+      <img style={{borderRadius: '0 200px 200px 200px'}} className="w-[120px]" src={image} alt="Popular Menu Image" />
       <div>
-        <h3>{name} </h3>
+        <h3 className="uppercase">{name}------------- </h3>
         <p>{recipe}</p>
       </div>
-      <p>{price}</p>
+      <p className="text-yellow-500">{price}</p>
+
     </div>
   );
 };
