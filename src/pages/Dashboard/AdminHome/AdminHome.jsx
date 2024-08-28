2532964d9a521ce72fbfdf1e1e5cce52f3617393
Shanto93 <shantoislam7363@gmail.com>
2024-08-28 11:21:34 +0600

    Admin Dashboard's responsiveness isses has been solved.Now it it responsive for all device

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
@@ -40,6 +41,55 @@ const AdminHome = () => {
     },
   });
 
+  const [chartSize, setChartSize] = useState({ width: 400, height: 400 });
+  const [piechartSize, setPieChartSize] = useState({ width: 400, height: 400 });
+
+  // Bara chart size
+  useEffect(() => {
+    const updateChartSize = () => {
+      if (window.innerWidth < 640) {
+        // Mobile devices
+        setChartSize({ width: 280, height: 220 });
+      } else if (window.innerWidth < 1024) {
+        // Tablets
+        setChartSize({ width: 400, height: 300 });
+      } else {
+        // PCs
+        setChartSize({ width: 450, height: 350 });
+      }
+    };
+
+    updateChartSize();
+    window.addEventListener("resize", updateChartSize);
+
+    return () => {
+      window.removeEventListener("resize", updateChartSize);
+    };
+  }, []);
+
+  // Pie Chart size
+  useEffect(() => {
+    const updatePieChartSize = () => {
+      if (window.innerWidth < 640) {
+        // Mobile devices
+        setPieChartSize({ width: 250, height: 250 });
+      } else if (window.innerWidth < 1024) {
+        // Tablets
+        setPieChartSize({ width: 400, height: 300 });
+      } else {
+        // PCs
+        setPieChartSize({ width: 350, height: 350 });
+      }
+    };
+
+    updatePieChartSize();
+    window.addEventListener("resize", updatePieChartSize);
+
+    return () => {
+      window.removeEventListener("resize", updatePieChartSize);
+    };
+  }, []);
+
   if (isLoading) {
     return (
       <>
@@ -55,7 +105,6 @@ const AdminHome = () => {
     return <div>Something went wrong. Please try again later.</div>;
   }
 
-  //Custom shape for the bar chart
   const getPath = (x, y, width, height) => {
     return `M${x},${y + height}C${x + width / 3},${y + height} ${
       x + width / 2
@@ -69,14 +118,13 @@ const AdminHome = () => {
 
   const TriangleBar = (props) => {
     const { fill, x, y, width, height } = props;
-
     return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
   };
 
-  //Custom Shape for Pie chart
-  const pieChartData = chartData.map((data) => {
-    return { name: data.category, value: data.revenue };
-  });
+  const pieChartData = chartData.map((data) => ({
+    name: data.category,
+    value: data.revenue,
+  }));
 
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red"];
 
@@ -107,102 +155,108 @@ const AdminHome = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h1 className="text-2xl font-semibold my-3">
-          Hi, Welcome {user.displayName ? user.displayName : "back"}!
-        </h1>
+    <div className="p-4">
+      <h1 className="text-xl md:text-2xl font-semibold mb-4">
+        Hi, Welcome {user.displayName ? user.displayName : "back"}!
+      </h1>
 
-        {/* Stats DIV */}
-        <div>
-          <div className="stats shadow">
-            <div className="stat">
-              <div className="stat-figure text-3xl text-orange-500">
-                <FaDollarSign></FaDollarSign>
-              </div>
-              <div className="stat-value">{stats.revenue} </div>
-              <div className="stat-title">Revenue</div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-3xl text-orange-500">
-                <FaUsers></FaUsers>
-              </div>
-              <div className="stat-value">{stats.customers} </div>
-              <div className="stat-title">Customers</div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-3xl text-orange-500">
-                <FaBook></FaBook>
-              </div>
-              <div className="stat-value">{stats.products} </div>
-              <div className="stat-title">Products</div>
-            </div>
-
-            <div className="stat">
-              <div className="stat-figure text-3xl text-orange-500">
-                <FaFirstOrder></FaFirstOrder>
-              </div>
-              <div className="stat-value">{stats.orders} </div>
-              <div className="stat-title">Orders</div>
-            </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="stat shadow">
+          <div className="stat-figure text-3xl text-orange-500">
+            <FaDollarSign />
           </div>
+          <div className="stat-value text-lg md:text-xl lg:text-2xl">
+            {stats.revenue}
+          </div>
+          <div className="stat-title">Revenue</div>
         </div>
 
-        {/* Chart div */}
-        <div className="flex mt-5">
-          <div className="w-1/2">
-            <BarChart
-              width={500}
-              height={300}
-              data={chartData}
-              margin={{
-                top: 20,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="category" />
-              <YAxis />
-              <Bar
-                dataKey="quantity"
-                fill="#8884d8"
-                shape={<TriangleBar />}
-                label={{ position: "top" }}
-              >
-                {chartData.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={colors[index % 6]} />
-                ))}
-              </Bar>
-            </BarChart>
+        <div className="stat shadow">
+          <div className="stat-figure text-3xl text-orange-500">
+            <FaUsers />
           </div>
+          <div className="stat-value text-lg md:text-xl lg:text-2xl">
+            {stats.customers}
+          </div>
+          <div className="stat-title">Customers</div>
+        </div>
 
-          {/* Pie Chart div */}
-          <div className="w-1/2">
-            <PieChart width={400} height={400}>
-              <Pie
-                data={pieChartData}
-                cx={200}
-                cy={200}
-                labelLine={false}
-                label={renderCustomizedLabel}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {pieChartData.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              <Legend></Legend>
-            </PieChart>
+        <div className="stat shadow">
+          <div className="stat-figure text-3xl text-orange-500">
+            <FaBook />
           </div>
+          <div className="stat-value text-lg md:text-xl lg:text-2xl">
+            {stats.products}
+          </div>
+          <div className="stat-title">Products</div>
+        </div>
+
+        <div className="stat shadow">
+          <div className="stat-figure text-3xl text-orange-500">
+            <FaFirstOrder />
+          </div>
+          <div className="stat-value text-lg md:text-xl lg:text-2xl">
+            {stats.orders}
+          </div>
+          <div className="stat-title">Orders</div>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 mt-5 md:gap-20 lg:grid-cols-2">
+        <div className="w-full">
+          <BarChart
+            width={chartSize.width}
+            height={chartSize.height}
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            className="mx-auto"
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="category" />
+            <YAxis />
+            <Bar
+              dataKey="quantity"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {chartData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+
+        <div className="w-full">
+          <PieChart
+            width={piechartSize.width}
+            height={piechartSize.height}
+            className="mx-auto"
+          >
+            <Pie
+              data={pieChartData}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius="80%"
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {pieChartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend />
+          </PieChart>
         </div>
       </div>
     </div>
